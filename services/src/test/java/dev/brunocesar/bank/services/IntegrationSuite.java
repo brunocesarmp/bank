c94b18a68c54f495e8ae5a6598899425a58aba88
Bruno Cesar <bcesar.bruno@gmail.com>
2021-03-29 14:26:42 -0300

    Created Integration Suite tests

---
+++ services/src/test/java/dev/brunocesar/bank/services/IntegrationSuite.java
@@ -0,0 +1,14 @@
+package dev.brunocesar.bank.services;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Bruno César - https://github.com/brunocesarmp
+ * @since 2021-03-29
+ */
+@RunWith(JUnitPlatform.class)
+@SelectPackages({"dev.brunocesar.bank.services"})
+public class IntegrationSuite {
+}
